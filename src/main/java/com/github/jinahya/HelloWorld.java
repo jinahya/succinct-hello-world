bdc5405b13717fed46282c6a6b5b0670cc521123
Jin Kwon <onacit@gmail.com>
2024-09-07 14:59:04 +0900

    initial commit

--- src/main/java/com/github/jinahya/HelloWorld.java
+++ src/main/java/com/github/jinahya/HelloWorld.java
@@ -1,6 +1,19 @@
 package com.github.jinahya;
 
+/**
+ * The main program of this module whose {@link #main(String...)} method prints {@code hello, world}, to the
+ * {@link System#out}, followed by a system dependency line separator.
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ */
 public class HelloWorld {
+
+    /**
+     * Prints {@code hello, world}, to the {@link System#out}, followed by a system dependency line separator.
+     *
+     * @param args command line arguments.
+     * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+     */
     public static void main(String... args) {
         System.out.printf("hello, world%n");
     }
--- src/test/java/com/github/jinahya/HelloWorldTest.java
+++ src/test/java/com/github/jinahya/HelloWorldTest.java
@@ -1,10 +1,39 @@
 package com.github.jinahya;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * A class for testing {@link HelloWorld} class.
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ */
 class HelloWorldTest {
 
+    /**
+     * Tests {@link HelloWorld#main(String...)} method.
+     *
+     * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+     */
     @Test
     void main__() {
+        final var out = System.out;
+        try {
+            // --------------------------------------------------------------------------------------------------- given
+            final var buffer = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(buffer));
+            // ---------------------------------------------------------------------------------------------------- when
+            HelloWorld.main();
+            // ---------------------------------------------------------------------------------------------------- then
+            final var actual = buffer.toByteArray();
+            final var expected = ("hello, world" + System.lineSeparator()).getBytes(StandardCharsets.US_ASCII);
+            Assertions.assertArrayEquals(actual, expected);
+        } finally {
+            System.setOut(out);
+        }
     }
 }
