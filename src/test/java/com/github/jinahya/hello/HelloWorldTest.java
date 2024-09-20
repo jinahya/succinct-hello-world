9a536a30ce7ac2f4c3554a4edc6e45a74ff1bf0e
Jin Kwon <onacit@gmail.com>
2024-09-20 14:22:05 +0900

    wip

--- src/test/java/com/github/jinahya/hello/HelloWorldTest.java
+++ src/test/java/com/github/jinahya/hello/HelloWorldTest.java
@@ -1,22 +1,38 @@
 package com.github.jinahya.hello;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 
 /**
  * A class for testing {@link HelloWorld} class.
  *
  * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
  */
-class HelloWorldTest {
+@SuppressWarnings({
+        "java:S5786" // JUnit5 test classes and methods should have default package visibility
+})
+public class HelloWorldTest {
 
+    /**
+     * Creates a new instance.
+     */
+    HelloWorldTest() {
+        super();
+    }
+
+    /**
+     * Asserts that the private constructor of the {@link HelloWorld} class throws an {@link AssertionError}.
+     *
+     * @throws NoSuchMethodException when failed to get the private constructor.
+     */
     @Test
-    void __() throws NoSuchMethodException {
+    public void __() throws NoSuchMethodException {
         final var constructor = HelloWorld.class.getDeclaredConstructor();
         constructor.setAccessible(true);
         final var thrown = Assertions.assertThrows(
@@ -29,12 +45,18 @@ class HelloWorldTest {
     }
 
     /**
-     * Tests {@link HelloWorld#main(String...)} method.
+     * Verifies that the {@link HelloWorld#main(String...)} method prints {@code hello, world}, to the
+     * {@link System#out}, followed by a system-dependent line separator.
      *
      * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+     * @see System#lineSeparator()
+     * @see <a
+     * href="https://stackoverflow.com/questions/79005325/what-is-the-proper-charset-for-decoding-systemlineseparator">What
+     * is the proper charset for decoding System#lineSeparator?</a> (Stackoverflow.com)
      */
+    @DisplayName("main(args) prints 'hello, world' followed by a system-dependent line separator")
     @Test
-    void main__() {
+    public void main_PrintHelloWorld_() {
         final var out = System.out;
         try {
             // --------------------------------------------------------------------------------------------------- given
@@ -44,7 +66,7 @@ class HelloWorldTest {
             HelloWorld.main();
             // ---------------------------------------------------------------------------------------------------- then
             final var actual = buffer.toByteArray();
-            final var expected = ("hello, world" + System.lineSeparator()).getBytes(StandardCharsets.US_ASCII);
+            final var expected = ("hello, world" + System.lineSeparator()).getBytes(Charset.defaultCharset());
             Assertions.assertArrayEquals(actual, expected);
         } finally {
             System.setOut(out);
