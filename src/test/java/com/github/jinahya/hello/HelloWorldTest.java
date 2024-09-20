28fb4a574a2923bdaca397d2bf341424da8df2a9
Jin Kwon <onacit@gmail.com>
2024-09-20 15:25:35 +0900

    wip

--- src/test/java/com/github/jinahya/hello/HelloWorldTest.java
+++ src/test/java/com/github/jinahya/hello/HelloWorldTest.java
@@ -3,16 +3,24 @@ package com.github.jinahya.hello;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.Charset;
+import java.util.stream.Stream;
 
 /**
  * A class for testing {@link HelloWorld} class.
  *
  * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ * @see <a
+ * href="https://stackoverflow.com/questions/79005325/what-is-the-proper-charset-for-decoding-systemlineseparator">What
+ * is the proper charset for decoding System#lineSeparator?</a> (Stackoverflow.com)
+ * @see <a href="https://github.com/junit-team/junit5/issues/2482">Add Assumptions.assumeDoesNotThrow</a>
+ * (github.com/junit-team/junit5/issues/2482)
  */
 @SuppressWarnings({
         "java:S5786" // JUnit5 test classes and methods should have default package visibility
@@ -50,9 +58,6 @@ public class HelloWorldTest {
      *
      * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
      * @see System#lineSeparator()
-     * @see <a
-     * href="https://stackoverflow.com/questions/79005325/what-is-the-proper-charset-for-decoding-systemlineseparator">What
-     * is the proper charset for decoding System#lineSeparator?</a> (Stackoverflow.com)
      */
     @DisplayName("main(args) prints 'hello, world' followed by a system-dependent line separator")
     @Test
@@ -61,12 +66,47 @@ public class HelloWorldTest {
         try {
             // --------------------------------------------------------------------------------------------------- given
             final var buffer = new ByteArrayOutputStream();
-            System.setOut(new PrintStream(buffer));
+            final var charset = Charset.defaultCharset();
+            System.setOut(new PrintStream(buffer, true, charset));
             // ---------------------------------------------------------------------------------------------------- when
             HelloWorld.main();
             // ---------------------------------------------------------------------------------------------------- then
             final var actual = buffer.toByteArray();
-            final var expected = (HelloWorld.STRING + System.lineSeparator()).getBytes(Charset.defaultCharset());
+            final var expected = (HelloWorld.STRING + System.lineSeparator()).getBytes(charset);
+            Assertions.assertArrayEquals(actual, expected);
+        } finally {
+            System.setOut(out);
+        }
+    }
+
+    /**
+     * Returns a stream of {@link Charset} which each {@link Charset#canEncode() can encode}.
+     *
+     * @return a stream of {@link Charset}.
+     */
+    private static Stream<Charset> getCharsetStream() {
+        return Charset.availableCharsets().values().stream().filter(Charset::canEncode);
+    }
+
+    /**
+     * Tests the {@link HelloWorld#main(String...)} method with specified charset.
+     *
+     * @param charset the charset to test with.
+     */
+    @DisplayName("main(args) prints 'hello, world' followed by a system-dependent line separator")
+    @MethodSource("getCharsetStream")
+    @ParameterizedTest
+    public void main_PrintHelloWorld_(final Charset charset) {
+        final var out = System.out;
+        try {
+            // --------------------------------------------------------------------------------------------------- given
+            final var buffer = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(buffer, true, charset));
+            // ---------------------------------------------------------------------------------------------------- when
+            HelloWorld.main();
+            // ---------------------------------------------------------------------------------------------------- then
+            final var actual = buffer.toByteArray();
+            final var expected = (HelloWorld.STRING + System.lineSeparator()).getBytes(charset);
             Assertions.assertArrayEquals(actual, expected);
         } finally {
             System.setOut(out);
