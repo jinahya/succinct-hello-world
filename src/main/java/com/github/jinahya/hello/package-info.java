e7466cd090624c834146ad0ad4ded19f08c55603
Jin Kwon <onacit@gmail.com>
2024-09-20 13:53:11 +0900

    wip

--- src/main/java/com/github/jinahya/hello/package-info.java
+++ src/main/java/com/github/jinahya/hello/package-info.java
@@ -1,6 +1,7 @@
 /**
- * Defines a succinct hello, world program.
+ * Defines a succinct {@code hello, world} program.
  *
  * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ * @see com.github.jinahya.hello.HelloWorld
  */
 package com.github.jinahya.hello;
